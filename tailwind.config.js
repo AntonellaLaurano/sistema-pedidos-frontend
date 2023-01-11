4a65bc43f07eb8b2b2bd8a040a13111a3f6ede78
Antonella Laurano <alaurano@shokworks.io>
2023-01-11 18:51:01 -0400

    base

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ["./components/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ]
+}
