9146964bbf7cf775ea1086d403ecd06e832adaed
DanielMora <danielalejandrolmoratorres@gmail.com>
2023-01-15 12:05:14 -0400

    changes waiting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,28 @@
 import { Layout } from 'components/layout';
+import Link from 'next/link';
+import styles from '../styles/index.module.css'
 
 const Home = () => {
   return (
-    <Layout>
-      Hola mundo!
-    </Layout>
+    // <Layout>
+      <div className={styles.container}>
+        {/* <img className={styles.image} src="/landing.jpeg" alt="landing" /> */}
+        <div className={styles.containerImage}>
+          <div className={styles.containerContent}>
+            <h1 className={styles.title}>Bienvenidos a Vigus Lunch!!!</h1>
+            <h3 className={styles.subtitle}>La tienda con los mejores pasteles y empanadas de toda la ciudad, con el menu mas variado</h3>
+            <div className={styles.containerButtons}>
+              <Link href="#">
+                <button className={styles.button}>Menu</button>
+              </Link>
+              <Link href="#">
+                <button className={styles.button}>Ordenar</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    // </Layout>
   )
 }
 
--- pages/waitingOrder.tsx
+++ pages/waitingOrder.tsx
@@ -8,8 +8,8 @@ const waitingOrder = () => {
       {/* <img className={styles.image} src="/landing.jpeg" alt="landing" /> */}
       <div className={styles.containerImage}>
         <div className={styles.containerContent}>
-          <h1 className={styles.title}>Bienvenidos a Vigus Lunch!!!</h1>
-          <h3 className={styles.subtitle}>La tienda con los mejores pasteles y empanadas de toda la ciudad, con el menu mas variado</h3>
+          <h1 className={styles.title}>Procesando su orden... Espere</h1>
+          <h3 className={styles.subtitle}>Lista de ordenes</h3>
           <div className={styles.containerButtons}>
             <button className={styles.button}>Menu</button>
             <button className={styles.button}>Ordenar</button>
