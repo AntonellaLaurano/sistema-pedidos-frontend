a1abfbac1bdb9e20d6cea25bd62ec91cf4961e39
DanielMora <danielalejandrolmoratorres@gmail.com>
2023-01-26 21:45:41 -0400

    responsive and cards

---
+++ components/orderCard/orderCard.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import styles from '../../styles/orderCard.module.css'
+
+const orders = [
+    {
+      image: '',
+      name: 'Pastele Carne Mechada',
+      price: 1,
+      quantity: 3,
+    },
+    {
+        image: '',
+        name: 'Pastele Carne Mechada',
+        price: 1,
+        quantity: 3,
+      },
+      {
+        image: '',
+        name: 'Pastele Carne Mechada',
+        price: 1,
+        quantity: 3,
+      },
+      {
+        image: '',
+        name: 'Pastele Carne Mechada',
+        price: 1,
+        quantity: 3,
+      },
+      {
+        image: '',
+        name: 'Pastele Carne Mechada',
+        price: 1,
+        quantity: 3,
+      },
+      {
+        image: '',
+        name: 'Pastele Carne Mechada',
+        price: 1,
+        quantity: 3,
+      },
+      {
+        image: '',
+        name: 'Pastele Carne Mechada',
+        price: 1,
+        quantity: 3,
+      },
+    // More people...
+  ]
+
+const orderCard = () => {
+    const [total, setTotal] = useState<number>(0);
+    useEffect(() => {
+        let cont = 0;
+        orders.map(({ price, quantity}) => {
+            cont = (price*quantity) + cont;
+        });
+        setTotal(cont);
+      }, []);
+  return (
+    <div className={styles.containerOrders}>
+        <p className={styles.total}>Total Price: {total}$</p>
+    {orders?.map(({ image, name, price, quantity }, index) => {
+        return (
+            <div className={styles.container} key={index}>
+            <div className={styles.imageContainer}></div>
+            <div className={styles.descriptionContainer}>
+              <p className={styles.title}>{name}</p>
+              <p className={styles.price}>{price}$</p>
+            </div>
+            <div className={styles.quantityTotalContainer}>
+              <p className={styles.quantity}>{quantity}</p>
+            </div>
+            <div className={styles.quantityTotalContainer}>
+              <p className={styles.total}>{price*quantity}$</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+export default orderCard
--- pages/waitingOrder.tsx
+++ pages/waitingOrder.tsx
@@ -2,18 +2,19 @@ import React from 'react'
 import styles from '../styles/waiting.module.css'
 import OrdersList from 'components/ordersList/ordersList';
 import Navbar from '../components/navbar/navbar';
+import OrderCard from '../components/orderCard/orderCard';
 
 const waitingOrder = () => {
   return (
-    <>
+    <div className={styles.section}>
       <Navbar />
-      <div className={styles.container} style={{width: '100vw', height: '100vh'}}>
+      <div className={styles.container}>
         {/* <img className={styles.image} src="/landing.jpeg" alt="landing" /> */}
         <div className={styles.containerImage}>
           <div className={styles.containerContent}>
             <h1 className={styles.title}>Procesando su orden... Espere</h1>
             <h3 className={styles.subtitle}>Lista de ordenes</h3>
-            <OrdersList></OrdersList>
+            <OrderCard></OrderCard>
             <div className={styles.containerButtons}>
               <button className={styles.button}>Menu</button>
               <button className={styles.button}>Ordenar</button>
@@ -21,7 +22,7 @@ const waitingOrder = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
